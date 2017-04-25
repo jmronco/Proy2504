e23d6bc830f318be48dc9a3f34c1a0c7272b8a27
cetecom <cetecom@LC08L5722>
2017-04-25 11:14:08 -0300

    Yippie

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>YIPPE</title>
+        <title>YIPPIE</title>
     </head>
     <body>
         <?php
